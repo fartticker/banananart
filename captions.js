67e5c7668a6c238a4f6158a9e2b41ad00d9350ac
fartticker <jhouchen99@gmail.com>
2025-05-26 10:46:21 -0600

    Add files via upload

---
+++ captions.js
@@ -0,0 +1,10 @@
+
+const captions = [
+  "This banana just got back from Basel.",
+  "Conceptually ripe.",
+  "A still life in chaos.",
+  "Minimalism, but make it potassium.",
+  "You wouldn’t peelieve it.",
+  "Post-post-bananaism.",
+  "Warhol weeps softly."
+];
